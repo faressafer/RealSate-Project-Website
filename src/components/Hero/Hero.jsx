c47194bb9d8e266ecc3a1271e40f7b368b872bc2
FaresOps <faressafer05@gmail.com>
2024-01-27 01:05:58 +0100

    Your commit message here

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import "./Hero.css";
+import { HiLocationMarker } from "react-icons/hi";
+import CountUp from "react-countup";
+
+const Hero = () => {
+  return (
+    <section className="hero-wrapper">
+      <div className="paddings innerWidth hero-container flexCenter">
+        {/* left side */}
+
+        <div className="flexColStart hero-left">
+          <div className="hero-title">
+            <div className="orange-circle" />
+            <h1>
+              Discover <br />
+              Most Suitable <br /> Property
+            </h1>
+          </div>
+          <div className="flexColStart hero-des">
+            <span className="secondaryText">
+              Find a variety of properties that suityou very easily
+            </span>
+            <span className="secondaryText">
+              forget all difficulties in finding a residence for you
+            </span>
+          </div>
+          <div className=" flexCenter search-bar">
+            <HiLocationMarker color="var(--blue)" size={25} />
+            <input type="text" />
+            <button className="button">Search</button>
+          </div>
+          <div className="flexCenter stats">
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp start={8800} end={9000} duration={4} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Priemum Prodcuts</span>
+            </div>
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp start={1950} end={2000} duration={4} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Happy Costumers</span>
+            </div>
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp end={9000} duration={4} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Award winnings</span>
+            </div>
+          </div>
+        </div>
+        {/* right side */}
+        <div className="hero-right">
+          <div className="image-container">
+            <img src="./hero-image.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
